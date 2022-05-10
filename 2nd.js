d299fcf3626595c8336fa5079c134a2275b2d655
DiegoRendon2001 <103530306+DiegoRendon2001@users.noreply.github.com>
2022-05-10 15:16:37 -0700

    Update 2nd.js

--- 2nd.js
+++ 2nd.js
@@ -1,31 +1,20 @@
-
-// our handy object constructor to create as many destination (trip) objects as we need.
-let Trip = function (pDestination, pMiles, pGallons) {
-    this.destination = pDestination;
-    this.miles = parseFloat(pMiles);
-    this.gallons = parseFloat(pGallons);
-
-    // I moved this function definition into the orginal constuctor
-    // just to show it can be done, but not a good idea
-    this.calculateMpg = function () {
-            return this.miles / this.gallons;
-    };
+let Movie = function(pTitle, pRating){
+    this.title = pTitle;
+    this.rating = parseInt(pRating);
 };
 
-// adding methods to the object outside of its defintion
-Trip.prototype.isValid = function () {
-    if (this.destination == "" ||
-        isNaN(this.miles) ||
-        isNaN(this.gallons)) {
+Movie.prototype.isValid = function(){
+    if(this.title == " " || isNaN(this.rating) ){
         return false;
-    } else if (this.miles <= 0 || this.gallons <= 0) {
+    }
+    else if (this.rating < 1 || this.rating > 5){
         return false;
-    } else {
+    }
+    else{
         return true;
     }
 };
 
-Trip.prototype.toString = function () {
-    let mpg = this.calculateMpg().toFixed(1);
-    return this.destination + ": Miles - " + this.miles + "; MPG - " + mpg;
+Movie.prototype.GetAll = function(){
+    return this.title + " with rating " + this.rating;
 };
